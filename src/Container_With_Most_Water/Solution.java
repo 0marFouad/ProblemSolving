2a8dca9cfa11ed1471421490b697d3d193bf5886
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2019-10-11 14:44:05 +0200

    Added Some Solutions

---
+++ src/Container_With_Most_Water/Solution.java
@@ -0,0 +1,31 @@
+package Container_With_Most_Water;
+
+public class Solution {
+
+    public static int calArea(int left,int right,int[] height){
+        return Math.min(height[left],height[right])*(right - left);
+    }
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int currentArea = calArea(left,right,height);
+        while(left < right){
+            int keepLeft = calArea(left,right-1,height);
+            int keepRight = calArea(left+1,right,height);
+            if(height[left] > height[right]){
+                currentArea = Math.max(currentArea,keepLeft);
+                right--;
+            }else{
+                currentArea = Math.max(currentArea,keepRight);
+                left++;
+            }
+        }
+        return currentArea;
+    }
+
+    public static void main(String[] args){
+        int[] input = {1,3,2,5,25,24,5};
+        System.out.println(maxArea(input));
+    }
+}
---
+++ src/Hamming_Distance/Solution.java
@@ -0,0 +1,13 @@
+package Hamming_Distance;
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x^y;
+        int result = 0;
+        while(xor != 0){
+            result += xor%2;
+            xor/=2;
+        }
+        return result;
+    }
+}
