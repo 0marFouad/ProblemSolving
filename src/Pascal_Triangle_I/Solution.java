989d35c0657c90490924869cce3a6e6724aa55c8
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2019-11-30 23:00:15 +0200

    Pascal

---
+++ src/Pascal_Triangle_I/Solution.java
@@ -0,0 +1,29 @@
+package Pascal_Triangle_I;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int nCr(int n, int r){
+        r = Math.min(r,n-r);
+        double ans=1;
+        for (int i = 0; i < r; ++i){
+            ans *= (n - i);
+            ans /= (i + 1);
+        }
+        return (int)ans;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                l.add(nCr(i,j));
+            }
+            res.add(l);
+        }
+        return res;
+    }
+
+}
