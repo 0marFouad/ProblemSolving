b3311445c953c3f1478cfe795a2befc9b8aa7eb2
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-06-22 17:48:59 +0200

    Faster Version of Matrix Block Sum

--- src/Matrix_Block_Sum/Solution.java
+++ src/Matrix_Block_Sum/Solution.java
@@ -20,9 +20,50 @@ public class Solution {
         return res;
     }
 
+    public static int[][] matrixBlockSumFaster(int[][] mat, int K) {
+        if(mat.length < 1 || K == 0){
+            return mat;
+        }
+        int[][] cum = new int[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                cum[i][j] = mat[i][j];
+                if(j>0){
+                    cum[i][j] += cum[i][j-1];
+                }
+            }
+        }
+        for(int i=1;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                cum[i][j] += cum[i-1][j];
+            }
+        }
+        int[][] res = new int[mat.length][mat[0].length];
+        for(int i=0;i<res.length;i++){
+            for(int j=0;j<res[0].length;j++){
+                int fromI = Math.max(0,i-K) - 1;
+                int fromJ = Math.max(0,j-K) - 1;
+                int toI = Math.min(mat.length-1,i + K);
+                int toJ = Math.min(mat[i].length-1,j + K);
+                res[i][j] = cum[toI][toJ];
+                if(fromI >= 0){
+                    res[i][j] -= cum[fromI][toJ];
+                }
+                if(fromJ >= 0){
+                    res[i][j] -= cum[toI][fromJ];
+                }
+                if(fromI >= 0 && fromJ >= 0){
+                    res[i][j] += cum[fromI][fromJ];
+                }
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args){
-        int[][] inp = {{1,2,3},{4,5,6},{7,8,9}};
-        int[][] out = matrixBlockSum(inp,2);
+        int[][] inp = {{67,64,78},{99,98,38},{82,46,46},{6,52,55},{55,99,45}};
+        int[][] out = matrixBlockSumFaster(inp,3);
         for(int i=0;i<out.length;i++){
             for (int j=0;j<out[i].length;j++){
                 System.out.print(out[i][j] + " ");
