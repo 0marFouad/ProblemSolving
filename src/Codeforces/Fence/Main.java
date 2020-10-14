79b2a6bd254e3c43420e593a5f4466e439f3b375
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-10-14 18:34:24 +0200

    Bargain is V.Imp

---
+++ src/Codeforces/Fence/Main.java
@@ -0,0 +1,83 @@
+package Codeforces.Fence;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args){
+        MyScanner s = new MyScanner();
+        int t = s.nextInt();
+        while (t-- > 0) {
+            long[] arr = new long[3];
+            for(int i=0;i<3;i++){
+                arr[i] = s.nextInt();
+            }
+            Arrays.sort(arr);
+            System.out.println(arr[2] - arr[0] + arr[1]);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+}
+
+
