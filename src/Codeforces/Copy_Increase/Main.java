c7e6cc25149763705b4e77d2ece7d778bbc8c980
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-10-16 02:10:07 +0200

    Increase Copy Imp

---
+++ src/Codeforces/Copy_Increase/Main.java
@@ -0,0 +1,81 @@
+package Codeforces.Copy_Increase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args){
+        MyScanner s = new Main.MyScanner();
+        int t = s.nextInt();
+        while(t-->0){
+            int n = s.nextInt();
+            int ans = 0;
+            int jump = 1;
+            int current = 1;
+            boolean flag = false;
+            while(current < n){
+                if(!flag){
+                    flag = true;
+                }else{
+                    jump++;
+                    flag = false;
+                }
+                current += jump;
+                ans++;
+            }
+            System.out.println(ans);
+        }
+    }
+
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+}
+
+
