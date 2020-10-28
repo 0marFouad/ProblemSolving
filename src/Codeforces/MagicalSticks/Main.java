acf6b5f4bb9f892dd1188cb7895781fefa585f58
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-10-28 04:11:22 +0200

    Magical Sticks

---
+++ src/Codeforces/MagicalSticks/Main.java
@@ -0,0 +1,67 @@
+package Codeforces.MagicalSticks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+
+    public static void main(String[] args){
+        MyScanner sc = new MyScanner();
+        int t = sc.nextInt();
+        while(t-->0){
+            long n = sc.nextInt();
+            System.out.println((n+1)/2);
+        }
+    }
+
+
+
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+}
---
+++ src/Codeforces/PermutationForgery/Main.java
@@ -0,0 +1,74 @@
+package Codeforces.PermutationForgery;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+
+    public static void main(String[] args){
+        MyScanner sc = new MyScanner();
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+            for(int i=n-1;i>=0;i--){
+                System.out.print(arr[i] +" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+}
