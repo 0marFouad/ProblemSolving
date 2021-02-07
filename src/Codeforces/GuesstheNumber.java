02c4b0ff56a6ebe2e54ed98f55232b6aab1545f4
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2021-02-07 14:33:26 +0200

    Testing Interactive

---
+++ src/Codeforces/GuesstheNumber.java
@@ -0,0 +1,85 @@
+package Codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class GuesstheNumber {
+
+    public static void main(String[] args){
+        MyScanner sc = new MyScanner();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        int l = 1;
+        int r = 1000000;
+        while(l!=r){
+            int mid = (r+l+1)/2;
+            printWriter.println(mid);
+            printWriter.flush();
+            String s = sc.next();
+            if(s.equals(">=")){
+                l=mid;
+            }else {
+                r = mid - 1;
+            }
+        }
+        printWriter.println("! "+l);
+        printWriter.flush();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+}
